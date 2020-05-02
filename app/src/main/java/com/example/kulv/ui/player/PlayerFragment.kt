7720294c7208c85b6bc07247d247ff607f84f94a
CHENTIA <tcchen@thoughtworks.com>
2020-05-02 13:22:37 +0800

    添加播放器界面.

---
+++ app/src/main/java/com/example/kulv/ui/player/PlayerFragment.kt
@@ -0,0 +1,62 @@
+package com.example.kulv.ui.player
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.kulv.R
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+
+
+class PlayerFragment : Fragment() {
+
+    private lateinit var playerViewModel: PlayerViewModel
+    private var player: ExoPlayer? = null
+    private lateinit var playerView: PlayerView
+    private val playWhenReady = true
+    private val currentWindow = 0
+    private val playbackPosition: Long = 0
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        playerViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(requireActivity().application))
+            .get(PlayerViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_player, container, false)
+        playerView = root.findViewById(R.id.player)
+        initializePlayer()
+        return root
+    }
+
+    private fun initializePlayer() {
+        if (player == null) {
+            player = ExoPlayerFactory.newSimpleInstance(
+                context,
+                DefaultTrackSelector()
+            )
+            playerView.player = player
+            player?.playWhenReady = playWhenReady
+            player?.seekTo(currentWindow, playbackPosition)
+        }
+        val uri: Uri = Uri.parse("http://music.163.com/song/media/outer/url?id=447925558.mp3")
+        val mediaSource: MediaSource = buildMediaSource(uri)
+        player?.prepare(mediaSource, false, true)
+    }
+
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        return ExtractorMediaSource.Factory(
+            DefaultHttpDataSourceFactory("exoplayer-codelab")
+        ).createMediaSource(uri)
+    }
+}
--- app/src/main/java/com/example/kulv/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/kulv/ui/player/PlayerViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.kulv.ui.home
+package com.example.kulv.ui.player
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class PlayerViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
