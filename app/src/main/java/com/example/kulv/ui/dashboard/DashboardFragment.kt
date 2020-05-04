bbdcf3a045252356b432c0dcaff8a8a016feca08
CHENTIA <tcchen@thoughtworks.com>
2020-05-04 13:35:18 +0800

    完成上一首和下一首功能.

--- app/src/main/java/com/example/kulv/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/kulv/ui/dashboard/DashboardFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.kulv.R
 import com.example.kulv.ui.player.BarChartView
@@ -12,7 +14,6 @@ import com.example.kulv.ui.player.BarChartView
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
-    private lateinit var barChartView: BarChartView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,12 +23,10 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        barChartView = root.findViewById(R.id.myRealMapView)
-        barChartView.start()
-//        val textView: TextView = root.findViewById(R.id.text_dashboard)
-//        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
         return root
     }
 }
--- app/src/main/java/com/example/kulv/ui/player/PlayerFragment.kt
+++ app/src/main/java/com/example/kulv/ui/player/PlayerFragment.kt
@@ -5,16 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.beardedhen.androidbootstrap.AwesomeTextView
 import com.example.kulv.R
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class PlayerFragment : Fragment() {
@@ -22,9 +27,12 @@ class PlayerFragment : Fragment() {
     private lateinit var playerViewModel: PlayerViewModel
     private var player: ExoPlayer? = null
     private lateinit var playerView: PlayerView
+    private lateinit var name: TextView
     private val playWhenReady = true
     private val currentWindow = 0
     private val playbackPosition: Long = 0
+
+    private val musicList: ArrayList<String> = ArrayList()
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -34,6 +42,7 @@ class PlayerFragment : Fragment() {
         playerViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(requireActivity().application))
             .get(PlayerViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_player, container, false)
+        name = root.findViewById(R.id.music_name)
         playerView = root.findViewById(R.id.player)
         initializePlayer()
         return root
@@ -49,10 +58,20 @@ class PlayerFragment : Fragment() {
             player?.playWhenReady = playWhenReady
             player?.seekTo(currentWindow, playbackPosition)
         }
+        musicList.add("http://vjs.zencdn.net/v/oceans.mp4")
+        musicList.add("https://media.w3.org/2010/05/sintel/trailer.mp4")
+        musicList.add("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+        musicList.add("http://music.163.com/song/media/outer/url?id=447925558.mp3")
 //        val uri: Uri = Uri.parse("http://music.163.com/song/media/outer/url?id=447925558.mp3")
-        val uri: Uri = Uri.parse("http://vjs.zencdn.net/v/oceans.mp4")
-        val mediaSource: MediaSource = buildMediaSource(uri)
+
+        val mediaSources = arrayOfNulls<MediaSource>(musicList.size)
+        for (i in musicList.indices) {
+            mediaSources[i] = buildMediaSource(Uri.parse(musicList[i]))
+        }
+        val mediaSource =
+            if (mediaSources.size == 1) mediaSources[0] else ConcatenatingMediaSource(*mediaSources)
         player?.prepare(mediaSource, false, true)
+
     }
 
     private fun buildMediaSource(uri: Uri): MediaSource {
